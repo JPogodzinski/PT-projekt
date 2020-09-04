e65ce0db39c765f93413978d79eb1d94751e4140
JPogodzinski <pogodzinskij@gmail.com>
2020-09-04 12:04:03 +0200

    init fun

--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -48,9 +48,48 @@ window.addEventListener( 'load', () => {
             } );
 
 
+            socket.on( 'ice candidates', async ( data ) => {
+                data.candidate ? await pc[data.sender].addIceCandidate( new RTCIceCandidate( data.candidate ) ) : '';
+            } );
+
+
+            socket.on( 'sdp', async ( data ) => {
+                if ( data.description.type === 'offer' ) {
+                    data.description ? await pc[data.sender].setRemoteDescription( new RTCSessionDescription( data.description ) ) : '';
+
+                    h.getUserFullMedia().then( async ( stream ) => {
+                        if ( !document.getElementById( 'local' ).srcObject ) {
+                            h.setLocalStream( stream );
+                        }
+
+                        //save my stream
+                        myStream = stream;
+
+                        stream.getTracks().forEach( ( track ) => {
+                            pc[data.sender].addTrack( track, stream );
+                        } );
+
+                        let answer = await pc[data.sender].createAnswer();
+
+                        await pc[data.sender].setLocalDescription( answer );
+
+                        socket.emit( 'sdp', { description: pc[data.sender].localDescription, to: data.sender, sender: socketId } );
+                    } ).catch( ( e ) => {
+                        console.error( e );
+                    } );
+                }
+
+                else if ( data.description.type === 'answer' ) {
+                    await pc[data.sender].setRemoteDescription( new RTCSessionDescription( data.description ) );
+                }
+            } );
 
 
             socket.on( 'chat', ( data ) => {
                 h.addChat( data, 'remote' );
             } );
         } );
+
+
+        function init( createOffer, partnerName ) {
+            }
