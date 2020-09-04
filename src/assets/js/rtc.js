f51fa232d6fc7592043d8575822deaca3b383eb1
JPogodzinski <pogodzinskij@gmail.com>
2020-09-04 11:54:10 +0200

    initialize vars

--- src/assets/js/rtc.js
+++ src/assets/js/rtc.js
@@ -1,1 +1,23 @@
 import h from './settings.js';
+
+window.addEventListener( 'load', () => {
+    const room = h.getQString( location.href, 'room' );
+    if ( !room ) {
+        document.querySelector( '#room-create' ).attributes.removeNamedItem( 'hidden' );
+    }
+    else {
+        let commElem = document.getElementsByClassName( 'room-comm' );
+
+        for ( let i = 0; i < commElem.length; i++ ) {
+            commElem[i].attributes.removeNamedItem( 'hidden' );
+        }
+
+        var pc = [];
+
+        let socket = io( '/stream' );
+
+        var socketId = '';
+        var myStream = '';
+        var screen = '';
+        //Get user video by default
+        getAndSetUserStream();
